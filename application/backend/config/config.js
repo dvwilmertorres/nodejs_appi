6d2a6de2478e65874c5952bdafce8269ef10e07a
dv.wilmer.torres@outlook.com <dv.wilmer.torres@outlook.com>
2024-06-07 11:38:34 -0500

    Actualizacion codigo

--- application/backend/config/config.js
+++ application/backend/config/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     mongodb: {
-        url: process.env.MONGODB_URL || 'mongodb://admin:admin@ec2-174-129-178-2.compute-1.amazonaws.com:27017/?tls=false&authSource=unirdb',
+        url: process.env.MONGODB_URL || 'mongodb://admin:admin@ec2-54-87-13-121.compute-1.amazonaws.com:27017/?tls=false&authSource=unirdb',
         dbName: process.env.MONGODB_DB_NAME || 'unirdb'
     }
 };
